eeb588ec153ac2adf36e656c702966543010a4a7
Luis Gaytan <gaytan_luis@msn.com>
2017-07-31 22:49:26 -0700

    first commit

---
+++ app/models/blog.rb
@@ -0,0 +1,13 @@
+# == Schema Information
+#
+# Table name: blogs
+#
+#  id         :integer          not null, primary key
+#  title      :string
+#  body       :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class Blog < ApplicationRecord
+end
