238273472805283bf5e3f5e1f536279e177e928d
Luis Gaytan <gaytan_luis@msn.com>
2017-08-01 17:57:39 -0700

    custom action for updating status

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,14 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
-  resources :blogs
+
+  resources :blogs do
+    member do
+      get :toggle_status
+    end
+  end
+
+
   resources :portfolios, except: [:show]
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
